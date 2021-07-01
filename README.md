81f982e50d08b89b3001be92c17e3d6fb62d966a
Jack Chapman <chapmanjack0777@gmail.com>
2021-07-01 03:04:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Coding Challenges
+This repository contains my solutions to programming challenges from websites such as 
+[HackerRank](https://www.hackerrank.com/domains/algorithms) and [LeetCode](https://leetcode.com/problemset/all/).
---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.5.20"
+    id("org.jetbrains.dokka") version "1.4.32"
+}
+
+group = "com.jackchapman"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+ 
